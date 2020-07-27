21bd0a18e0acb8a2f662268a98158a5861d06092
Pyyxel <ax.vassaux@gmail.com>
2020-07-27 08:26:53 +0200

    /stat/match/(id)

--- src/Controller/StatMatchController.php
+++ src/Controller/StatMatchController.php
@@ -14,8 +14,6 @@ class StatMatchController extends AbstractController
      */
     public function index(MatchRepository $Match, MatchTypeRepository $Type,$id)
     {   
-        
-
         //affiche team local
         $team = $Match->find($id);
         $local=$team->getLocalTeam();
@@ -29,15 +27,32 @@ class StatMatchController extends AbstractController
         //affiche score
         $tmp=$team->getStats();
         $score=$tmp[0]["score"];
+        //affiche carton rouge
+        $red=$tmp[1]["red"];
+        //affiche carton jaune
+        $yellow=$tmp[2]["yellow"];
+        //affiche essais
+        $essais=$tmp[3]["essais"];
+        //affiche transformation
+        $trans=$tmp[4]["transformation"];
+        //affiche penalite
+        $penalite=$tmp[5]["penalite"];
+        //affiche drops
+        $drops=$tmp[6]["drops"];
        
-        
     
         return $this->render('stat_match/index.html.twig', [
-            
             'local_team'=>$local,
             'visitor_team'=>$visitor,
             'match_type'=>$type,
-            'score'=>$score
+            'score'=>$score,
+            'red'=>$red,
+            'yellow'=>$yellow,
+            'essais'=>$essais,
+            'transformation'=>$trans,
+            'penalite'=>$penalite,
+            'drops'=>$drops
+            
         ]);
     }
 }
