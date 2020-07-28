479f2894eb958170475f981c3caa0e296f29e173
Steven-Blombou <blombou@simplon-charleville.fr>
2020-07-28 11:57:28 +0200

    commit authen ern cours

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,10 +8,9 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
-
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="L'Email que vous avez indiqué est déjà utilisé !")
  */
 class User implements UserInterface
 {
@@ -35,6 +34,7 @@ class User implements UserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * 
      */
     private $password;
 
