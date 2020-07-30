0b34c7578360bb14e4fbeb6c3ba3c538cccc1c69
Guillaume <poucetguillaume@gmail.com>
2020-07-30 16:27:02 +0200

    modal css

--- src/Controller/TeamScreenController.php
+++ src/Controller/TeamScreenController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Team;
+use App\Form\FormAddTeamType;
 use App\Repository\TeamRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\ORM\EntityManagerInterface;
 
 class TeamScreenController extends AbstractController
 {
@@ -16,11 +18,35 @@ class TeamScreenController extends AbstractController
      */
 
     
-    public function index(TeamRepository $teamRepo, TeamRepository $team, UserRepository $user)
+    public function index(Request $request, TeamRepository $teamRepo, TeamRepository $team, UserRepository $user)
     {
         $id = 0;
         
-        
+        $team = new Team();
+
+        $form = $this->createForm(FormAddTeamType::class, $team);
+
+        $form->handleRequest($request);
+        // if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            $team->addUser($this->getUser());
+    
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($team);
+            $entityManager->flush();
+
+
+    
+            // return $this->redirectToRoute('task_success');
+
+        }
+
         $teams = $teamRepo->findAll();
         // dump($teams);
 
@@ -31,7 +57,8 @@ class TeamScreenController extends AbstractController
         return $this->render('team_screen/index.html.twig', [
             'controller_name' => 'TeamScreenController',
             'teams' => $teams,
-            'team' => $team
+            'team' => $team,
+            'form' => $form->createView(),
         ]);
     }
 }
