a300c58bef7288b7a2646333417c3d03b69d16ee
JJJ <jean.julien.jupinet@gmail.com>
2020-08-05 16:12:20 +0200

    fix matchcalendar vide

--- src/Controller/MatchCalendarController.php
+++ src/Controller/MatchCalendarController.php
@@ -21,83 +21,88 @@ class MatchCalendarController extends AbstractController
         $match=$team->find($id_team)->getPlayMatches();
         $match_id=$team->find($id_team);
 
-       
+       $resultat = [];
+       $stat = [];
 
         $userConnect = $this->getUser();
         $userClub=$user->find($userConnect)->getFinances()->getId();
         $club=$clubRepo->find($userClub)->getName();
         $count=$match->count($club);
         
-        $y=-1;
-        for($i=$count;$i>$count-5;$i--){
-            $y++;
-           
-            $local=$team->find($id_team)->getPlayMatches()[$y]->getLocalTeam();
-            if($club==$local){
-               if(($team->find($id_team)->getPlayMatches()[$y]->getStats()!=NULL) && (isset($score[0]['score']))){
+        if($count > 0) {
+
+            $y=-1;
+            for($i=$count;$i>$count-5;$i--){
+                $y++;
+               
+                $local=$team->find($id_team)->getPlayMatches()[$y]->getLocalTeam();
+                if($club==$local){
+                   if(($team->find($id_team)->getPlayMatches()[$y]->getStats()!=NULL) && (isset($score[0]['score']))){
+                        $score=$team->find($id_team)->getPlayMatches()[$y]->getStats();
+                        $score=$score[0]['score'];
+                        $score1=$score{0};
+                        $score2=$score{2};
+                   
+                    
+                   
+                        if($score1>$score2){
+                        
+                            $resultat[$i]=1;
+                        }else if($score1<$score2){
+                        
+                            $resultat[$i]=2;
+                        }else{
+                        
+                            $resultat[$i]=0;
+                        }
+                    }
+                }else{
                     $score=$team->find($id_team)->getPlayMatches()[$y]->getStats();
-                    $score=$score[0]['score'];
-                    $score1=$score{0};
-                    $score2=$score{2};
-               
+                   
+                    if(isset($score[0]["score"])){
+                        
+                        $score=$score[0]["score"];
+                        $score1=$score{0};
+                        $score2=$score{2};
+                        if($score2>$score1){
+                           
+                            $resultat[$i]=1;
+        
+                        }else if($score2<$score1){
+                            
+                            $resultat[$i]=2;
+                        }else{
+                            
+                            $resultat[$i]=0;
+                        }
+                    }
                 
-               
-                    if($score1>$score2){
                     
-                        $resultat[$i]=1;
-                    }else if($score1<$score2){
-                    
-                        $resultat[$i]=2;
-                    }else{
-                    
-                        $resultat[$i]=0;
-                    }
                 }
-            }else{
-                $score=$team->find($id_team)->getPlayMatches()[$y]->getStats();
-               
-                if(isset($score[0]["score"])){
-                    
-                    $score=$score[0]["score"];
-                    $score1=$score{0};
-                    $score2=$score{2};
-                    if($score2>$score1){
-                       
-                        $resultat[$i]=1;
-    
-                    }else if($score2<$score1){
-                        
-                        $resultat[$i]=2;
-                    }else{
-                        
-                        $resultat[$i]=0;
-                    }
-                }
-            
                 
             }
+            $z=2;
+            $i=0;
+            
+            while($z!=0){
+                if(isset($team->find($id_team)->getPlayers()[$i])){
+                    $joueur[$i] = $team->find($id_team)->getPlayers()[$i];
+                    $stat[$i][0]=$joueur[$i]->getLestName();
+                    $stat[$i][1]=$joueur[$i]->getFirstName();
+                    $stat[$i][2]=$joueur[$i]->getEssais()*5+$joueur[$i]->getTransformations()*2+$joueur[$i]->getDrops()*3+$joueur[$i]->getPenalites()*3;
+                    
+                    $i++;
+                    if($i==5){
+                        $z=0;
+                    }
+                }else{
+                    $z=0;
+                }  
+            }
+            arsort($stat);
             
         }
-        $z=2;
-        $i=0;
-        
-        while($z!=0){
-            if(isset($team->find($id_team)->getPlayers()[$i])){
-                $joueur[$i] = $team->find($id_team)->getPlayers()[$i];
-                $stat[$i][0]=$joueur[$i]->getLestName();
-                $stat[$i][1]=$joueur[$i]->getFirstName();
-                $stat[$i][2]=$joueur[$i]->getEssais()*5+$joueur[$i]->getTransformations()*2+$joueur[$i]->getDrops()*3+$joueur[$i]->getPenalites()*3;
-                
-                $i++;
-                if($i==5){
-                    $z=0;
-                }
-            }else{
-                $z=0;
-            }  
-        }
-        arsort($stat);
-        
+
 
 
         
