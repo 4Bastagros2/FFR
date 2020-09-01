bc855de4d5aef25d8e5c67e8d165e5fa134934de
JJJ <jean.julien.jupinet@gmail.com>
2020-09-01 16:02:38 +0200

    Match multi compo

--- src/Controller/MatchCompositionController.php
+++ src/Controller/MatchCompositionController.php
@@ -15,9 +15,9 @@ class MatchCompositionController extends AbstractController
     private $composition;
 
     /**
-     * @Route("/match/composition/{id_match}", name="match_composition")
+     * @Route("/match/composition/{id_match}/{id_composition}", name="match_composition", defaults={"id_composition"=0})
      */
-    public function index(MatchRepository $matchRep, $id_match)
+    public function index(MatchRepository $matchRep, $id_match, $id_composition)
     {
         $date=$matchRep->find($id_match)->getDate()->format('Y-m-d H:i:s');
         $today=date('Y-m-d H:i:s');
@@ -48,29 +48,30 @@ class MatchCompositionController extends AbstractController
 
         $players = $team->getPlayers();
 
-        $bench = [];
+        // $bench = [];
 
-        foreach ($players as $player)
-        {
-                if(!in_array($player->getId(), $compo))
-                {
-                    // dump('player in bench'.$player->getId());
-                    $bench[] = $player;
-                }
-        }
+        // foreach ($players as $player)
+        // {
+        //         if(!in_array($player->getId(), $compo))
+        //         {
+        //             // dump('player in bench'.$player->getId());
+        //             $bench[] = $player;
+        //         }
+        // }
 
         // dump($players);
         // dump($bench);
         // dump(json_encode($bench));
         
-
+        
+        // 'bench' => $bench,
         $return = $this->render('match_composition/index.html.twig', [
             'controller_name' => 'MatchCompositionController',
             'id_match' => $id_match,
             'players' => $players,
-            'bench' => $bench,
             'composition' => json_encode($compo),
             'idteam' => $id_team,
+            'idcomposition' => $id_composition,
         ]);
     }
     return $return;
@@ -81,18 +82,28 @@ class MatchCompositionController extends AbstractController
      * @Route("/match/composition/update/{id_match}", name="match_composition_update")
      */
     public function update(Request $request, MatchRepository $matchRep, $id_match) {
-        $entityManager = $this->getDoctrine()->getManager();
-        
-        dump($request);
-        $match=$matchRep->find($id_match);
-        $compo = json_decode($request->request->get('composition'));
-        $match->setComposition($compo);
-        dump($compo);
-        
-        $entityManager->persist($match);
-        $entityManager->flush();
+        // if ($request->isXmlHttpRequest() ||  $request->request->get('composition') == 1)
+        // {
 
-        $response = new Response();
-        return $response;
-      }
+            
+        //     $entityManager = $this->getDoctrine()->getManager();
+            
+        //     dump($request);
+        //     $match=$matchRep->find($id_match);
+        //     // $compo = json_decode($request->request->get('composition'));
+        //     $compo = $request->request->get('composition');
+        //     dump($composition);
+        //     // $compo = json_decode('[{"banc":[36,32],"compo":[0,0,0,0,17,37,0,32,0,0,0,0,0,0,0,0],"selected":[1]},{"banc":[0,0,0,0,0,0,0],"compo":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"selected":[]},{"banc":[0,0,0,0,0,0,0],"compo":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"selected":[]}]');
+        //     // $match->setComposition($compo);
+        //     dump($compo);
+            
+        //     $entityManager->persist($match);
+        //     $entityManager->flush();
+
+            $response = new Response();
+        //     $response->setContent(json_encode($compo));
+            return $response;
+        // }
+
+    }
 }
