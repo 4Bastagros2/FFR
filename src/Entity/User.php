ed8a3bc4bb6d5a3fb510abfd0a05588801fbe481
Steven-Blombou <blombou@simplon-charleville.fr>
2020-09-01 21:17:51 +0200

    commit fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,8 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @UniqueEntity(fields={"email"}, message="L'Email que vous avez indiqué est déjà utilisé !")
  */
-// class User implements UserInterface
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -23,17 +22,7 @@ class User
     private $id;
 
     /**
-     * @ORM\OneToMany(targetEntity=Message::class, mappedBy="user")
-     */
-    private $messages;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Participant::class, mappedBy="user")
-     */
-    private $participants;
-
-    /**
-     * @ORM\Column(type="string", length=180)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
@@ -43,221 +32,35 @@ class User
     private $roles = [];
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     * 
      */
     private $password;
 
     /**
-     * @ORM\ManyToMany(targetEntity=Team::class, mappedBy="users")
+     * @ORM\ManyToMany(targetEntity=Team::class, inversedBy="users")
      */
     private $coaches;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Club::class, inversedBy="users")
+     */
+    private $finances;
 
-    // /**
-    //  * @ORM\ManyToOne(targetEntity=Club::class, inversedBy="users")
-    //  */
-    // private $finances;
-
-    // /**
-    //  * @ORM\Column(type="boolean")
-    //  */
-    // private $isVerified = false;
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
-        // $this->coaches = new ArrayCollection();
-        $this->messages = new ArrayCollection();
-        $this->participants = new ArrayCollection();
+        $this->coaches = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    // public function getEmail(): ?string
-    // {
-    //     return $this->email;
-    // }
-
-    // public function setEmail(string $email): self
-    // {
-    //     $this->email = $email;
-
-    //     return $this;
-    // }
-
-    // /**
-    //  * A visual identifier that represents this user.
-    //  *
-    //  * @see UserInterface
-    //  */
-    // public function getUsername(): string
-    // {
-    //     return (string) $this->email;
-    // }
-
-    // /**
-    //  * @see UserInterface
-    //  */
-    // public function getRoles(): array
-    // {
-    //     $roles = $this->roles;
-    //     // guarantee every user at least has ROLE_USER
-    //     $roles[] = 'ROLE_USER';
-
-    //     return array_unique($roles);
-    // }
-
-    // public function setRoles(array $roles): self
-    // {
-    //     $this->roles = $roles;
-
-    //     return $this;
-    // }
-
-    // /**
-    //  * @see UserInterface
-    //  */
-    // public function getPassword(): string
-    // {
-    //     return (string) $this->password;
-    // }
-
-    // public function setPassword(string $password): self
-    // {
-    //     $this->password = $password;
-
-    //     return $this;
-    // }
-
-    // /**
-    //  * @see UserInterface
-    //  */
-    // public function getSalt()
-    // {
-    //     // not needed when using the "bcrypt" algorithm in security.yaml
-    // }
-
-    // /**
-    //  * @see UserInterface
-    //  */
-    // public function eraseCredentials()
-    // {
-    //     // If you store any temporary, sensitive data on the user, clear it here
-    //     // $this->plainPassword = null;
-    // }
-
-    // /**
-    //  * @return Collection|Team[]
-    //  */
-    // public function getCoaches(): Collection
-    // {
-    //     return $this->coaches;
-    // }
-
-    // public function addCoach(Team $coach): self
-    // {
-    //     if (!$this->coaches->contains($coach)) {
-    //         $this->coaches[] = $coach;
-    //     }
-
-    //     return $this;
-    // }
-
-    // public function removeCoach(Team $coach): self
-    // {
-    //     if ($this->coaches->contains($coach)) {
-    //         $this->coaches->removeElement($coach);
-    //     }
-
-    //     return $this;
-    // }
-
-    // public function getFinances(): ?Club
-    // {
-    //     return $this->finances;
-    // }
-
-    // public function setFinances(?Club $finances): self
-    // {
-    //     $this->finances = $finances;
-
-    //     return $this;
-    // }
-
-    // public function isVerified(): bool
-    // {
-    //     return $this->isVerified;
-    // }
-
-    // public function setIsVerified(bool $isVerified): self
-    // {
-    //     $this->isVerified = $isVerified;
-
-    //     return $this;
-    // }
-
-    /**
-     * @return Collection|Message[]
-     */
-    public function getMessages(): Collection
-    {
-        return $this->messages;
-    }
-
-    public function addMessage(Message $message): self
-    {
-        if (!$this->messages->contains($message)) {
-            $this->messages[] = $message;
-            $message->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMessage(Message $message): self
-    {
-        if ($this->messages->contains($message)) {
-            $this->messages->removeElement($message);
-            // set the owning side to null (unless already changed)
-            if ($message->getUser() === $this) {
-                $message->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Participant[]
-     */
-    public function getParticipants(): Collection
-    {
-        return $this->participants;
-    }
-
-    public function addParticipant(Participant $participant): self
-    {
-        if (!$this->participants->contains($participant)) {
-            $this->participants[] = $participant;
-            $participant->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeParticipant(Participant $participant): self
-    {
-        if ($this->participants->contains($participant)) {
-            $this->participants->removeElement($participant);
-            // set the owning side to null (unless already changed)
-            if ($participant->getUser() === $this) {
-                $participant->setUser(null);
-            }
-        }
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -272,9 +75,26 @@ class User
         return $this;
     }
 
-    public function getRoles(): ?array
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->roles;
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): self
@@ -284,9 +104,12 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
     {
-        return $this->password;
+        return (string) $this->password;
     }
 
     public function setPassword(string $password): self
@@ -294,6 +117,23 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     /**
@@ -308,7 +148,6 @@ class User
     {
         if (!$this->coaches->contains($coach)) {
             $this->coaches[] = $coach;
-            $coach->addUser($this);
         }
 
         return $this;
@@ -318,8 +157,31 @@ class User
     {
         if ($this->coaches->contains($coach)) {
             $this->coaches->removeElement($coach);
-            $coach->removeUser($this);
         }
+
+        return $this;
+    }
+
+    public function getFinances(): ?Club
+    {
+        return $this->finances;
+    }
+
+    public function setFinances(?Club $finances): self
+    {
+        $this->finances = $finances;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
